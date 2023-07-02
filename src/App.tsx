10f5246de25687010cfe4c73e77e87acc1b45a64
Joe Liao <joe83830@gmail.com>
2023-07-02 14:05:50 -0400

    Drivers showing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Drivers from "./Drivers";
+import "./App.scss";
 
 export default function Application() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Drivers />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Drivers />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
--- src/Drivers.tsx
+++ src/Drivers.tsx
@@ -1,4 +1,42 @@
+import { AgGridReact } from "ag-grid-react";
+import { ColDef } from "ag-grid-community";
+import { useState, useEffect } from "react";
+import './Drivers.scss';
+
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
 export default function Drivers() {
-    console.log("JOE")
-    return <h1>Drivers</h1>;
+    const [driversData, setDriversData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("https://localhost:7077/drivers");
+            const data = await response.json();
+            setDriversData(data)
+        };
+        fetchData();
+    }, []);
+
+
+    const columnDefs: ColDef[] = [
+        { field: "forename" },
+        { field: "surname" },
+        { field: "nationality" },
+        { field: "driverId" },
+        { field: "driverRef" },
+        { field: "number" },
+        { field: "dob" },
+        { field: "code" }
+    ];
+
+    return (
+        <div className="ag-theme-alpine grid-container">
+            <AgGridReact
+                className="inner-grid"
+                rowData={driversData}
+                columnDefs={columnDefs}
+            ></AgGridReact>
+        </div>
+    );
 }
